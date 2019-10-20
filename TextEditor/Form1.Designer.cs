ec7a296434e8f215225048b533aeeb67c734a025
Dmytro Mykytenko <50519414+dmykyten@users.noreply.github.com>
2019-10-20 17:44:46 +0300

    Initial commit

---
+++ TextEditor/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TextEditor
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.mainPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonsPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.mainTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.mainPanel.SuspendLayout();
+            this.buttonsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.ColumnCount = 1;
+            this.mainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainPanel.Controls.Add(this.buttonsPanel, 0, 1);
+            this.mainPanel.Controls.Add(this.mainTextBox, 0, 0);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.RowCount = 2;
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+            this.mainPanel.Size = new System.Drawing.Size(1003, 567);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // buttonsPanel
+            // 
+            this.buttonsPanel.ColumnCount = 3;
+            this.buttonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.buttonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 107F));
+            this.buttonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 107F));
+            this.buttonsPanel.Controls.Add(this.openButton, 0, 0);
+            this.buttonsPanel.Controls.Add(this.saveButton, 1, 0);
+            this.buttonsPanel.Controls.Add(this.closeButton, 2, 0);
+            this.buttonsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonsPanel.Location = new System.Drawing.Point(3, 503);
+            this.buttonsPanel.Name = "buttonsPanel";
+            this.buttonsPanel.Padding = new System.Windows.Forms.Padding(10);
+            this.buttonsPanel.RowCount = 1;
+            this.buttonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.buttonsPanel.Size = new System.Drawing.Size(997, 61);
+            this.buttonsPanel.TabIndex = 0;
+            // 
+            // openButton
+            // 
+            this.openButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.openButton.Location = new System.Drawing.Point(683, 13);
+            this.openButton.MaximumSize = new System.Drawing.Size(87, 35);
+            this.openButton.MinimumSize = new System.Drawing.Size(87, 35);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(87, 35);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.saveButton.Location = new System.Drawing.Point(790, 13);
+            this.saveButton.MaximumSize = new System.Drawing.Size(87, 35);
+            this.saveButton.MinimumSize = new System.Drawing.Size(87, 35);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(87, 35);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.closeButton.Location = new System.Drawing.Point(897, 13);
+            this.closeButton.MaximumSize = new System.Drawing.Size(87, 35);
+            this.closeButton.MinimumSize = new System.Drawing.Size(87, 35);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(87, 35);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTextBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mainTextBox.Location = new System.Drawing.Point(5, 10);
+            this.mainTextBox.Margin = new System.Windows.Forms.Padding(5, 10, 5, 3);
+            this.mainTextBox.Multiline = true;
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.mainTextBox.Size = new System.Drawing.Size(993, 487);
+            this.mainTextBox.TabIndex = 1;
+            this.mainTextBox.TextChanged += new System.EventHandler(this.mainTextBox_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 567);
+            this.Controls.Add(this.mainPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(411, 263);
+            this.Name = "mainForm";
+            this.Text = "Text Editor";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.buttonsPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel mainPanel;
+        private System.Windows.Forms.TableLayoutPanel buttonsPanel;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.TextBox mainTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ TextEditor/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+    public partial class mainForm : Form
+    {
+        bool isFileChanged = false;
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            var windowResult = openFileDialog1.ShowDialog();
+            if (windowResult == DialogResult.OK)
+            {
+                var fileName = openFileDialog1.FileName;
+                mainTextBox.Text = File.ReadAllText(fileName.ToString());
+                isFileChanged = false;
+            }
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            var windowResult = saveFileDialog1.ShowDialog();
+
+            if (windowResult == DialogResult.OK)
+            {
+                var fileName = saveFileDialog1.FileName;
+                TextWriter txt = new StreamWriter(fileName);
+                txt.Write(mainTextBox.Text);
+                txt.Close();
+                isFileChanged = false;
+            }
+        }
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            if (!isFileChanged)
+            {
+                this.Close();
+            }
+            var result = MessageBox.Show("File has not been saved. Exit?", "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void mainTextBox_TextChanged(object sender, EventArgs e)
+        {
+            isFileChanged = true;
+        }
+    }
+}

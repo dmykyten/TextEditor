d0df71533c8a237211cfa689e06fd71bc4084fa4
Dmytro Mykytenko <dmykyten@gmail.com>
2021-09-05 17:10:42 +0300

    Added logging using NLog

--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -8,11 +8,13 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace TextEditor
 {
     public partial class mainForm : Form
     {
+        private static Logger formLogger = LogManager.GetCurrentClassLogger();
         bool isFileChanged = false;
         public mainForm()
         {
@@ -21,37 +23,56 @@ namespace TextEditor
 
         private void openButton_Click(object sender, EventArgs e)
         {
+            formLogger.Info("Clicked Open button...");
             var windowResult = openFileDialog1.ShowDialog();
             if (windowResult == DialogResult.OK)
             {
                 var fileName = openFileDialog1.FileName;
                 mainTextBox.Text = File.ReadAllText(fileName.ToString());
                 isFileChanged = false;
+                formLogger.Info($"Opened file {fileName}");
             }
         }
         private void saveButton_Click(object sender, EventArgs e)
         {
+            formLogger.Info("Clicked Save button...");
             var windowResult = saveFileDialog1.ShowDialog();
-
             if (windowResult == DialogResult.OK)
             {
                 var fileName = saveFileDialog1.FileName;
-                TextWriter txt = new StreamWriter(fileName);
-                txt.Write(mainTextBox.Text);
-                txt.Close();
+                formLogger.Info("Trying to write to file...");
+                try
+                {
+                    TextWriter txt = new StreamWriter(fileName);
+                    txt.Write(mainTextBox.Text);
+                    txt.Close();
+                    formLogger.Info("Wrote to file successfully");
+                }
+                catch (IOException ex)
+                {
+                    formLogger.Error(ex, "File is unaccessible.");
+                    var errorMessage = MessageBox.Show("File is unaccessible. Please, select other file to write.", "Access denied", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                    
+                }
                 isFileChanged = false;
             }
         }
         private void closeButton_Click(object sender, EventArgs e)
         {
+            formLogger.Info("Close button clicked...");
             if (!isFileChanged)
             {
                 this.Close();
+                formLogger.Info("File closed");
             }
-            var result = MessageBox.Show("File has not been saved. Exit?", "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-            if (result == DialogResult.Yes)
+            else
             {
-                this.Close();
+                var result = MessageBox.Show("File has not been saved. Exit?", "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    this.Close();
+                    formLogger.Info("File closed");
+                }
             }
         }
 
--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace TextEditor
 {
     static class Program
     {
+        private static Logger mainLogger = LogManager.GetCurrentClassLogger();
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            mainLogger.Info("App started");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new mainForm());
+            mainLogger.Info("App closed.");
         }
     }
 }
